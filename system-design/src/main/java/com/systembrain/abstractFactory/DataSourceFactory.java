c3f42863c25b7ca49adfa76aa12ffaf70b459c8f
VigneshKT <vigneshkatravath@gmail.com>
2022-05-28 22:31:59 +0530

    Refactored DataSourceFactory class replaced with switch case

--- system-design/src/main/java/com/systembrain/abstractFactory/DataSourceFactory.java
+++ system-design/src/main/java/com/systembrain/abstractFactory/DataSourceFactory.java
@@ -2,10 +2,16 @@ package com.systembrain.abstractFactory;
 
 public class DataSourceFactory {
 
-    public IngestData getInstance(String source){
-        if (source.equals("API")) return new SourceDataFromAPI();
-        else if (source.equals("DATABASE")) return new SourceDataFromDatabase();
-        else if (source.equals("FILE")) return new SourceDataFromFile();
-        return null;
+    public IngestData getInstance(String source) {
+        switch (source) {
+            case "API":
+                return new SourceDataFromAPI();
+            case "DATABASE":
+                return new SourceDataFromDatabase();
+            case "FILE":
+                return new SourceDataFromFile();
+            default:
+                return null;
+        }
     }
 }
